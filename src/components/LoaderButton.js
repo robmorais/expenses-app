6eea6f1726cc98a5a67cd729b373641a57b790d9
Roberto Silva <roberto@felloway.com>
2020-04-02 12:03:28 +0200

    created a loader button component that shows a loading icon on loading.

---
+++ src/components/LoaderButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Button, Spinner } from "react-bootstrap";
+import "./LoaderButton.css";
+
+export default function LoaderButton({
+  isLoading,
+  className = "",
+  disabled = false,
+  ...props
+}) {
+  return (
+    
+    <Button
+      className={`LoaderButton ${className}`}
+      disabled={disabled || isLoading}
+      {...props}
+    >
+      {isLoading ? <Spinner as="div" animation="border" size="sm" role="status" aria-hidden="true" /> : ""}
+      {props.children}
+    </Button>
+  );
+}
