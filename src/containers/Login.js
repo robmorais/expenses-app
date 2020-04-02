2103cfacb071cd588d832863804ad1e559760772
Roberto Silva <roberto@felloway.com>
2020-04-02 12:05:23 +0200

    Replaced Button with LoaderButton

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import "./Login.css";
 import API from "../utils/API";
+import Session from "../utils/Session";
+import LoaderButton from "../components/LoaderButton";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -13,6 +16,7 @@ export default function Login(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
     // Send login request
     API.post('sign_in', 
       {
@@ -27,7 +31,10 @@ export default function Login(props) {
         console.log(token);
         API.defaults.headers.common['X-User-Email'] = email;
         API.defaults.headers.common['X-User-Token'] = token;
+        Session.setSession(email, token);
         props.userHasAuthenticated(true);
+        setIsLoading(true);
+        props.history.push("/");
         // X-User-Email:carolcota@gmail.com
         //X-User-Token:19yt7qhrPRkcf4p--LF3
         // API.interceptors.request.use((config) => {
@@ -41,10 +48,13 @@ export default function Login(props) {
         //   console.log(err);
         //   return Promise.reject(err);
         // });
-      }, (err) => {console.log(err)});
-    // Create interceptor
-    // Register loggedIn, ? on state
-    // On interceptor, 
+      }, (err) => {
+        console.log(err);
+        setIsLoading(false);
+      }
+    );
+    // Create interceptor ?
+    // Setup session?
 
   }
 
@@ -68,9 +78,18 @@ export default function Login(props) {
             type="password"
           />
         </Form.Group>
-        <Button block size="large" disabled={!validateForm()} type="submit" >
+        {/* <Button block size="large" disabled={!validateForm()} type="submit" >
           Login
-        </Button>
+        </Button> */}
+        <LoaderButton
+          block
+          type="submit"
+          size="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Login
+        </LoaderButton>
       </Form>
     </div>
   );
