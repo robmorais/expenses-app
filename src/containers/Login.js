79536f5325ddef3ecc02ff48b334b1146f38695c
Roberto Silva <roberto@felloway.com>
2020-04-07 18:03:44 +0200

    refactor: clean up

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,53 +1,45 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
 import "./Login.css";
 import API from "../utils/API";
+import { useFormFields } from "../libs/hooksLib";
 import Session from "../utils/Session";
+import { Form } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 
 export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     setIsLoading(true);
     // Send login request
+    API.defaults.withCredentials = true;
+        
     API.post('sign_in', 
       {
         "user": {
-          "email": email,
-          "password":password
+          "email": fields.email,
+          "password":fields.password
         }
       })
       .then((response) => {
         console.log(response);
         const token = response.data.data.user.authentication_token;
         console.log(token);
-        API.defaults.headers.common['X-User-Email'] = email;
+        API.defaults.headers.common['X-User-Email'] = fields.email;
         API.defaults.headers.common['X-User-Token'] = token;
-        Session.setSession(email, token);
+        Session.setSession(fields.email, token);
         props.userHasAuthenticated(true);
         setIsLoading(true);
         props.history.push("/");
-        // X-User-Email:carolcota@gmail.com
-        //X-User-Token:19yt7qhrPRkcf4p--LF3
-        // API.interceptors.request.use((config) => {
-        //   console.log(config);
-        //   config[''] = token
-        //   //originalRequest['Authorization'] = 'Bearer ' + token;
-        //   // if (tokenIsExpired && path_is_not_login) {
-        //   // }
-        //   return config;
-        // }, (err) => {
-        //   console.log(err);
-        //   return Promise.reject(err);
-        // });
       }, (err) => {
         console.log(err);
         setIsLoading(false);
@@ -66,15 +58,15 @@ export default function Login(props) {
           <Form.Control
             autoFocus
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <Form.Group controlId="password" size="large">
         <Form.Label>Password</Form.Label>
           <Form.Control
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
             type="password"
           />
         </Form.Group>
