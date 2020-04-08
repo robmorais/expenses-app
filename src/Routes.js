c80d6c2dfb7ff0d7e59c8c4af197b00490d7482a
Roberto Silva <roberto@felloway.com>
2020-04-08 16:55:12 +0200

    added new transaction and summary routes.

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
 import Login from "./containers/Login"
 import NotFound from "./containers/NotFound"
 import AppliedRoute from "./components/AppliedRoute";
+import Transactions from "./containers/Transactions";
+import NewTransaction from "./containers/NewTransaction";
+import Summary from "./containers/Summary";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
-      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/" exact component={Transactions} appProps={appProps} />
       <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/transaction/new" exact component={NewTransaction} appProps={appProps} />
+      <AppliedRoute path="/summary" exact component={Summary} appProps={appProps} />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
